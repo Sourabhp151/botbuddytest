21ceea317396bbb753d9277a67acc0c498b3ed0c
akhilkandari2501 <165754031+akhilkandari2501@users.noreply.github.com>
2024-12-24 11:52:06 +0530

    Update UserContext.tsx
    admin user

--- amplify-chat-admin/app/projects/UserContext.tsx
+++ amplify-chat-admin/app/projects/UserContext.tsx
@@ -21,7 +21,7 @@ function reducer(state, action) {
                 isLoggedIn: true,
                 user: action.payload.user,
                 emailId: action.payload.emailId,
-                isAdmin: action.payload.emailId == "tiwvika@amazon.com"
+                isAdmin: action.payload.emailId == "akhil.kandari@redingtongroup.com"
             }
         case 'LOGOUT':
             handleSignOut();
